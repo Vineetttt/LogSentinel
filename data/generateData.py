ef5d49bbf9e22bb1c95dc4216604c8cbd374c0c2
Vineetttt <vineetchotaliya30@gmail.com>
2023-08-15 23:24:37 +0530

    generating synthetic dataset-v2

--- data/generateData.py
+++ data/generateData.py
@@ -9,38 +9,33 @@ import os
 os.environ['TZ'] = 'Asia/Kolkata'
 fak = faker.Faker()
 
-def str_time_prop(start, end, format, prop):
-    stime = time.mktime(time.strptime(start, format))
-    etime = time.mktime(time.strptime(end, format))
-    ptime = stime + prop * (etime - stime)
-    return time.strftime(format, time.localtime(ptime))
-
-def random_date(start, end, prop):
-    return str_time_prop(start, end, '%d/%b/%Y:%I:%M:%S %z', prop)
+def generate_random_timestamp(start_date, end_date):
+    time_diff = end_date - start_date
+    random_time = start_date + timedelta(seconds=random.randint(0, time_diff.total_seconds()))
+    return random_time
 
 
 dictionary = {
     'ips':[
         '192.168.1.1',
         '192.168.1.2',
-        '192.168.1.3',
-        '192.168.1.4',
-        '192.168.1.5',
-        '192.168.1.6',
+        '192.168.1.3'
     ],
-    'request': ['GET', 'POST', 'PUT', 'DELETE'], 
+    'dev_request': ['GET', 'POST','PUT'],
+    'analyst_request':['GET'],
+    'admin_request':['GET', 'POST','PUT', 'DELETE'], 
     'endpoint':[
-        '/api/v1/users',
         '/api/v1/products',
         '/api/v1/orders',
         '/api/v1/inventory',
+    ],
+    'admin_endpoint':[
         '/api/v1/payments',
-        '/api/v1/analytics',
         '/api/v1/dashboard',
         '/api/v1/configuration',
-    ], 
-    'statuscode': ['303', '404', '500', '403', '502', '304','200','401'], 
-    'username': ['james', 'adam', 'eve', 'alex', 'smith', 'bella', 'david', 'angela', 'millie', 'hilary','jamie','emma'],
+    ] ,
+    'statuscode': ['303', '404', '500', '403', '502', '304','200','401','200','200','200'], 
+    'usertype': ['developer','analyst','admin'],
     'ua' : ['Firefox/84.0',
             'Android/84.0',
             'Chrome/87.0',
@@ -49,36 +44,45 @@ dictionary = {
             'Safari',
             'iPhone; CPU OS 12_4_9',
         ],
-    'referrer' : ['-',fak.uri()*10]}
+}
 
-csv_file_path = 'data.csv'
+csv_file_path = 'data/data.csv'
 start_date = datetime(2023, 1, 1)
-end_date = datetime(2023, 3, 1)
+end_date = datetime(2023, 1, 5)
 
 data = []
 
-for _ in range(1, 100001):
+for _ in range(1, 10001):
     compliant = [True] * 90 + [False] * 10
     compliant_choice = random.choice(compliant)
+    time_stamp = generate_random_timestamp(start_date, end_date)
+    ua = random.choice(dictionary['ua'])
+    sc = random.choice(dictionary['statuscode'])
+    res = str(int(random.randint(1,1000)))
     if compliant_choice:
         ip = random.choice(dictionary['ips'])
-        ua = random.choice(dictionary['ua'])
+        ut = random.choice(dictionary['usertype'])
+        if ut == 'admin':
+            endpoint = random.choice(dictionary['admin_endpoint'])
+            rt = random.choice(dictionary['admin_request'])
+        elif ut == 'developer':
+            endpoint = random.choice(dictionary['endpoint'])
+            rt = random.choice(dictionary['dev_request'])
+        else:
+            endpoint = random.choice(dictionary['endpoint'])
+            rt = random.choice(dictionary['analyst_request'])      
     else:
-        ip = fak.ipv4()
-        us = fak.user_agent()
-            
-    date_time = start_date + timedelta(seconds=random.randint(0, int((end_date - start_date).total_seconds())))
-    request = random.choice(dictionary['request'])
-    endpoint = random.choice(dictionary['endpoint'])
-    status_code = random.choice(dictionary['statuscode'])
-    response_size = str(int(random.gauss(5000, 50)))
-    referrer = random.choice(dictionary['referrer'])
-    data.append([ip, date_time, request, endpoint, status_code, response_size, ua, referrer, compliant_choice])
+        ip = random.choice([fak.ipv4()]*995 + dictionary['ips']*5)
+        ut = random.choice(['develOper','analyst','admiin','deve1oper','devloper','analyzt','aadmin','developer','analyst'])
+        endpoint = random.choice(dictionary['admin_endpoint']*2 + dictionary['endpoint'])
+        rt = random.choice(dictionary['admin_request']*2+dictionary['dev_request']+dictionary['analyst_request'])
+
+    data.append([ip, time_stamp,ut,ua,endpoint,rt,sc,res,compliant_choice])
 
 sorted_data = sorted(data, key=lambda x: x[1])
 with open(csv_file_path, 'w', newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
-    header = ['IP Address', 'Date/Time', 'Request', 'Endpoint', 'Status Code', 'Response Size', 'User Agent', 'Referrer', 'Compliant']
+    header = ['IP Address', 'Timestamp', 'User Type','User Agent','Endpoint','Request Type','Status Code', 'Response Time','Compliant']
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(header)
     csv_writer.writerows(sorted_data)
