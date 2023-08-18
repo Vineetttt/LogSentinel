2ffc6b4eea6a1fa1b283bddeb30c16e8e86746d8
Vineetttt <vineetchotaliya30@gmail.com>
2023-08-18 23:06:57 +0530

    synthetic dataset minor chnages -v3.1

--- data/generate_prompt_data.py
+++ data/generate_prompt_data.py
@@ -1,16 +1,24 @@
 import pandas as pd
+import random
 
-def convert_to_prompt(rules,path,name):
+def convert_to_prompt(rules_list, path, name):
     df = pd.read_csv(path)
-    data = pd.DataFrame(columns=["rule","entry","label"])
+    data = pd.DataFrame(columns=["rule", "entry", "label"])
     prompts = []
+    
     for index, row in df.iterrows():
         prompt = ' '.join([f"{column} is {row[column]}" for column in df.columns if column != 'Compliant'])
         prompts.append(prompt)
 
-    data['rule'] = [rules]*len(prompts)
+    rules = []
+    for _ in range(len(prompts)):
+        chosen_rule = random.choice(rules_list)
+        rules.append(chosen_rule)
+
+    data['rule'] = rules
     data['entry'] = prompts
     data['label'] = df['Compliant']
-    data['label']=data['label'].astype(int)
-    csv_file_path = 'data/prompted_data/'+name+'.csv'
+    data['label'] = data['label'].astype(int)
+    
+    csv_file_path = 'data/prompted_data/' + name + '.csv'
     data.to_csv(csv_file_path, index=False)
